eff6b499b628dc2c7c740a30cda263b73f0ec281
GABRIEL ROMBOLA <rombolagabriel@gmail.com>
2022-12-31 16:35:22 -0300

    manage.py

--- ejemplo_dos/views.py
+++ ejemplo_dos/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 def index(request):
     return render(request, "ejemplo_dos/index.html", {})
 
-
-    
+class PostList(TemplateView):
+	template_name = "ejemplo_dos/post_list.html"
