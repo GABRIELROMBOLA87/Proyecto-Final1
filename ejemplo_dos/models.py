12f8de7bfcdc8a5f347f3480761a4f4ce323bcd5
GABRIEL ROMBOLA <rombolagabriel@gmail.com>
2023-01-09 01:34:37 -0300

    manage.py

--- ejemplo_dos/models.py
+++ ejemplo_dos/models.py
@@ -6,6 +6,5 @@ class Post(models.Model):
     Repuesto = models.CharField(max_length=100)
     Vehiculo = models.CharField(max_length=100)
     Detalle = models.TextField(max_length=3000)
-    #Precio = models.CharField(max_length=10)
-
+    Precio = models.IntegerField()
 
