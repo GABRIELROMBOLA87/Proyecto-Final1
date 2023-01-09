be03e709b1738ab00483eb20e0d11dc5a7c3f5a4
GABRIEL ROMBOLA <rombolagabriel@gmail.com>
2023-01-09 00:39:32 -0300

    manage.py

--- ejemplo_dos/models.py
+++ ejemplo_dos/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    titulo = models.CharField(max_length=100)
-    sub_titulo = models.CharField(max_length=100)
-    texto = models.TextField(max_length=3000)
-    publicado_el = models.DateField()
+    Repuesto = models.CharField(max_length=100)
+    Vehiculo = models.CharField(max_length=100)
+    Detalle = models.TextField(max_length=3000)
+    #Precio = models.CharField(max_length=10)
 
 
